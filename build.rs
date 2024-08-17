5d33ba846c8d9d76816389084abf6f1172127a35
Enrico Zandomeni Borba <enricozb@gmail.com>
2024-08-17 15:18:23 +0200

    precompile syntax-set.bin
    Instead of manually compiling each sublime-syntax file on load,
    syntect supports pre-compiling a group of syntax files. During
    compilation, a syntax-set.bin file is created that is included
    in the binary and loaded at runtime.

---
+++ build.rs
@@ -0,0 +1,20 @@
+use syntect::parsing::{SyntaxDefinition, SyntaxSetBuilder};
+
+// Example custom build script.
+fn main() {
+  // Tell Cargo that if the given file changes, to rerun this build script.
+  println!("cargo::rerun-if-changed=syntax/include");
+
+  let mut builder = SyntaxSetBuilder::new();
+
+  for file in std::fs::read_dir("syntax/include").unwrap() {
+    let file = file.unwrap().path();
+
+    if let Some("sublime-syntax") = file.extension().and_then(|ext| ext.to_str()) {
+      let syntax = std::fs::read_to_string(&file).unwrap();
+      builder.add(SyntaxDefinition::load_from_str(&syntax, false, None).unwrap());
+    }
+  }
+
+  syntect::dumps::dump_to_file(&builder.build(), "syntax/bin/syntax-set.bin").unwrap();
+}
